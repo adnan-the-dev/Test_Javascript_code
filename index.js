2842f127dad208b9bba596845eac890cac65ee26
adnan <azizart320480@gmail.com>
2024-01-06 22:30:46 +0500

    some

---
+++ index.js
@@ -0,0 +1,40 @@
+const array = [
+    {
+        name:'Special Pizza',
+        url:'https://whitescastle.netlify.app/images/menu/white-special-large.jpg'
+    },
+    {
+        name:'Appetizers',
+        url:'https://whitescastle.netlify.app/images/menu/tempilee-rolls.jpg'
+    },
+    {
+        name:'Extra',
+        url:'https://whitescastle.netlify.app/images/menu/dip-sauce.jpg'
+    },
+    {
+        name:'Beverages',
+        url:'https://whitescastle.netlify.app/images/menu/coca-cola.jpg'
+    },
+    {
+        name:'Pasta',
+        url:'https://whitescastle.netlify.app/images/menu/crunchy-pasta.jpg'
+    },
+    {
+        name:'Traditional Pizza',
+        url:'https://whitescastle.netlify.app/images/menu/euro.jpg'
+    },
+    {
+        name:'Square Pizza',
+        url:'https://whitescastle.netlify.app/images/menu/square-pizza.jpg'
+    },
+    {
+        name:'Crust House',
+        url:'https://whitescastle.netlify.app/images/menu/crown-crust.jpg'
+    },
+    {
+        name:'Deals',
+        url:'https://whitescastle.netlify.app/images/menu/special-classic.jpg'
+    },
+]
+
+console.log(array);
